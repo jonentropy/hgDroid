e73f19e24f81dfa4ca69e7d000aa4689fc1b0459
Tristan Linnell <talinnell@gmail.com>
2011-05-27 17:50:12 +0100

    Added crap song audio sample and media playback class.

--- gen/org/canthack/tris/android/hgdroid/R.java
+++ gen/org/canthack/tris/android/hgdroid/R.java
@@ -14,44 +14,47 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int btnCrapSong=0x7f080003;
-        public static final int btnQueueSong=0x7f080000;
-        public static final int lvNowPlaying=0x7f080002;
-        public static final int mitmQuit=0x7f080005;
-        public static final int mitmSettings=0x7f080004;
-        public static final int tvNowPlaying=0x7f080001;
+        public static final int btnCrapSong=0x7f090003;
+        public static final int btnQueueSong=0x7f090000;
+        public static final int lvNowPlaying=0x7f090002;
+        public static final int mitmQuit=0x7f090005;
+        public static final int mitmSettings=0x7f090004;
+        public static final int tvNowPlaying=0x7f090001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
     public static final class menu {
-        public static final int menu=0x7f070000;
+        public static final int menu=0x7f080000;
+    }
+    public static final class raw {
+        public static final int crapsong=0x7f050000;
     }
     public static final class string {
-        public static final int app_desc=0x7f050012;
-        public static final int app_name=0x7f050001;
-        public static final int crap_song=0x7f050014;
-        public static final int enable_ssl=0x7f05000c;
-        public static final int nowplaying=0x7f050013;
-        public static final int password_desc=0x7f050011;
-        public static final int password_string=0x7f050010;
-        public static final int port_desc=0x7f050009;
-        public static final int port_string=0x7f050008;
-        public static final int quit_label=0x7f05000a;
-        public static final int quit_shortcut=0x7f05000b;
-        public static final int server_desc=0x7f050007;
-        public static final int server_string=0x7f050006;
-        public static final int settings_label=0x7f050003;
-        public static final int settings_shortcut=0x7f050005;
-        public static final int settings_title=0x7f050004;
-        public static final int song_button=0x7f050002;
-        public static final int ssl_desc=0x7f05000d;
-        public static final int username_desc=0x7f05000f;
-        public static final int username_string=0x7f05000e;
-        public static final int welcome=0x7f050000;
+        public static final int app_desc=0x7f060012;
+        public static final int app_name=0x7f060001;
+        public static final int crap_song=0x7f060014;
+        public static final int enable_ssl=0x7f06000c;
+        public static final int nowplaying=0x7f060013;
+        public static final int password_desc=0x7f060011;
+        public static final int password_string=0x7f060010;
+        public static final int port_desc=0x7f060009;
+        public static final int port_string=0x7f060008;
+        public static final int quit_label=0x7f06000a;
+        public static final int quit_shortcut=0x7f06000b;
+        public static final int server_desc=0x7f060007;
+        public static final int server_string=0x7f060006;
+        public static final int settings_label=0x7f060003;
+        public static final int settings_shortcut=0x7f060005;
+        public static final int settings_title=0x7f060004;
+        public static final int song_button=0x7f060002;
+        public static final int ssl_desc=0x7f06000d;
+        public static final int username_desc=0x7f06000f;
+        public static final int username_string=0x7f06000e;
+        public static final int welcome=0x7f060000;
     }
     public static final class style {
-        public static final int LightThemeSelector=0x7f060000;
+        public static final int LightThemeSelector=0x7f070000;
     }
     public static final class xml {
         public static final int settings=0x7f040000;
--- src/org/canthack/tris/android/hgdroid/Status.java
+++ src/org/canthack/tris/android/hgdroid/Status.java
@@ -6,19 +6,33 @@
 
 package org.canthack.tris.android.hgdroid;
 
+import org.canthack.tris.android.media.SoundEffects;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class Status extends Activity {
+public class Status extends Activity implements OnClickListener{
+	private static final String TAG = "STATUS";
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        //Set all button's event handlers to the Activity itself...
+        View crapButton = this.findViewById(R.id.btnCrapSong);
+        crapButton.setOnClickListener(this);
+        
+        //TODO add other buttons and views here...
     }
     
     //Creates menus
@@ -33,6 +47,7 @@ public class Status extends Activity {
     //Handles menu clicks
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+    	Log.d("MENUS", "MENUS");
     	switch(item.getItemId()) {
     		case R.id.mitmSettings:
     			startActivity(new Intent(this, Settings.class));
@@ -40,14 +55,26 @@ public class Status extends Activity {
     		case R.id.mitmQuit:
     			finish();
     			break;
-    		//ToDo: add more menu items here...	
+    		//TODO: add more menu items here...	
     			
     	}
     	return false;
     }
-    
-    //ToDo: add button handlers...
-    
-    
-    
+
+    //Handle button clicks etc.
+	@Override
+	public void onClick(View v) {		
+		switch(v.getId()) {
+		case R.id.btnCrapSong:
+			//TODO add proper logic here. just a test for now
+			Toast t = Toast.makeText(this , R.string.crap_song, Toast.LENGTH_SHORT);
+			t.setGravity(Gravity.BOTTOM, 0, 0);
+			t.show();
+				
+			SoundEffects.playEffect(this, R.raw.crapsong);	
+			break;
+		//TODO Add more buttons here...
+			
+		}	
+	}      
 }
